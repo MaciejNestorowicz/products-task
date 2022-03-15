07d99171f8af98e99872b0ede1c535da106f641a
maciek <macko9909@gmail.com>
2022-03-15 18:23:37 +0100

    fixed access issues

--- db_utils/init.sql
+++ db_utils/init.sql
@@ -1,4 +1,8 @@
-CREATE TABLE products (
+UPDATE mysql.user SET Grant_priv='Y', Super_priv='Y' WHERE User='root';
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS products_db;
+USE products_db;
+CREATE TABLE IF NOT EXISTS products (
 	name VARCHAR(100) NOT NULL UNIQUE PRIMARY KEY,
 	amountInStock INT UNSIGNED NOT NULL
 )
